272d7450459020cac19ad78f4d5df30423aa1adf
lanjinger/liuyungen <>
2021-01-20 11:35:14 +0800

    增加dbRecorder的打印日志

--- dbrecorder2skywalking.go
+++ dbrecorder2skywalking.go
@@ -3,6 +3,9 @@ package cls_skywalking_client_go
 import (
 	"github.com/Masterminds/squirrel"
 	"github.com/Masterminds/structable"
+	"fmt"
+	"os"
+	"log"
 )
 
 type RecordProxy struct {
@@ -25,27 +28,104 @@ func (f RecordProxy) getRecorder() structable.Recorder {
 }
 
 func (f *RecordProxy) Insert() error {
-	return f.getRecorder().Insert()
+	queryStr := fmt.Sprintf("Insert table %s, whereIds: %v", f.Recorder.TableName(), f.Recorder.WhereIds())
+	reqSpan, spanErr := StartSpantoSkyWalkingForDb(queryStr, os.Getenv("DB_URL"))
+	if spanErr != nil {
+		log.Printf("StartSpantoSkyWalkingForDb insert error: %v \n", spanErr)
+	}
+
+	err := f.getRecorder().Insert()
+
+	if err != nil {
+		EndSpantoSkywalkingForDb(reqSpan, queryStr, false, err)
+	} else {
+		EndSpantoSkywalkingForDb(reqSpan, queryStr, true, nil)
+	}
+	return err
 }
 
 func (f *RecordProxy) Delete() error {
-	return f.getRecorder().Delete()
+	queryStr := fmt.Sprintf("Delete table %s, whereIds: %v", f.Recorder.TableName(), f.Recorder.WhereIds())
+	reqSpan, spanErr := StartSpantoSkyWalkingForDb(queryStr, os.Getenv("DB_URL"))
+	if spanErr != nil {
+		log.Printf("StartSpantoSkyWalkingForDb Delete error: %v \n", spanErr)
+	}
+
+	err := f.getRecorder().Delete()
+
+	if err != nil {
+		EndSpantoSkywalkingForDb(reqSpan, queryStr, false, err)
+	} else {
+		EndSpantoSkywalkingForDb(reqSpan, queryStr, true, nil)
+	}
+	return err
 }
 
 func (f *RecordProxy) Update() error {
-	return f.getRecorder().Update()
+	queryStr := fmt.Sprintf("Update table %s, whereIds: %v", f.Recorder.TableName(), f.Recorder.WhereIds())
+	reqSpan, spanErr := StartSpantoSkyWalkingForDb(queryStr, os.Getenv("DB_URL"))
+	if spanErr != nil {
+		log.Printf("StartSpantoSkyWalkingForDb Update error: %v \n", spanErr)
+	}
+	err := f.getRecorder().Update()
+
+	if err != nil {
+		EndSpantoSkywalkingForDb(reqSpan, queryStr, false, err)
+	} else {
+		EndSpantoSkywalkingForDb(reqSpan, queryStr, true, nil)
+	}
+	return err
 }
 
 func (f *RecordProxy) Load() error {
-	return f.getRecorder().Load()
+	queryStr := fmt.Sprintf("Load table %s, whereIds: %v", f.Recorder.TableName(), f.Recorder.WhereIds())
+	reqSpan, spanErr := StartSpantoSkyWalkingForDb(queryStr, os.Getenv("DB_URL"))
+	if spanErr != nil {
+		log.Printf("StartSpantoSkyWalkingForDb Load error: %v \n", spanErr)
+	}
+
+	err := f.getRecorder().Load()
+
+	if err != nil {
+		EndSpantoSkywalkingForDb(reqSpan, queryStr, false, err)
+	} else {
+		EndSpantoSkywalkingForDb(reqSpan, queryStr, true, nil)
+	}
+	return err
 }
 
 func (f *RecordProxy) LoadWhere(arg1 interface{}, arg2 ...interface{}) error {
-	return f.getRecorder().LoadWhere(arg1, arg2...)
+	queryStr := fmt.Sprintf("LoadWhere table %s, whereIds: %v", f.Recorder.TableName(), f.Recorder.WhereIds())
+	reqSpan, spanErr := StartSpantoSkyWalkingForDb(queryStr, os.Getenv("DB_URL"))
+	if spanErr != nil {
+		log.Printf("StartSpantoSkyWalkingForDb LoadWhere error: %v \n", spanErr)
+	}
+
+	err := f.getRecorder().LoadWhere(arg1, arg2...)
+
+	if err != nil {
+		EndSpantoSkywalkingForDb(reqSpan, queryStr, false, err)
+	} else {
+		EndSpantoSkywalkingForDb(reqSpan, queryStr, true, nil)
+	}
+	return err
 }
 
 func (f *RecordProxy) ExistsWhere(arg1 interface{}, arg2 ...interface{}) (bool, error) {
-	return f.getRecorder().ExistsWhere(arg1, arg2...)
+	queryStr := fmt.Sprintf("ExistsWhere table %s, whereIds: %v", f.Recorder.TableName(), f.Recorder.WhereIds())
+	reqSpan, spanErr := StartSpantoSkyWalkingForDb(queryStr, os.Getenv("DB_URL"))
+	if spanErr != nil {
+		log.Printf("StartSpantoSkyWalkingForDb ExistsWhere error: %v \n", spanErr)
+	}
+
+	boolValue, err := f.getRecorder().ExistsWhere(arg1, arg2...)
+
+	if err != nil {
+		EndSpantoSkywalkingForDb(reqSpan, queryStr, false, err)
+	} else {
+		EndSpantoSkywalkingForDb(reqSpan, queryStr, true, nil)
+	}
+	return boolValue, err
 }
 
 func (f *RecordProxy) Columns(boolean bool) []string {
@@ -57,67 +137,19 @@ func (f *RecordProxy) FieldReferences(boolean bool) []interface{} {
 }
 
 func (f *RecordProxy) Exists() (bool, error) {
-	return f.getRecorder().Exists()
+	queryStr := fmt.Sprintf("Exists table %s, whereIds: %v", f.Recorder.TableName(), f.Recorder.WhereIds())
+	reqSpan, spanErr := StartSpantoSkyWalkingForDb(queryStr, os.Getenv("DB_URL"))
+	if spanErr != nil {
+		log.Printf("StartSpantoSkyWalkingForDb Exists error: %v \n", spanErr)
+	}
+
+	boolValue, err := f.getRecorder().Exists()
+
+	if err != nil {
+		EndSpantoSkywalkingForDb(reqSpan, queryStr, false, err)
+	} else {
+		EndSpantoSkywalkingForDb(reqSpan, queryStr, true, nil)
+	}
+	return boolValue, err
 }
 
-//func (f RecordProxy) Insert() (*sql.Rows, error) {
-//	queryStr, args, _ := query.ToSql()
-//
-//	var temp = make([]string, len(args))
-//	for k, v := range args {
-//		temp[k] = fmt.Sprintf("%d", v)
-//	}
-//	var result = "[" + strings.Join(temp, ",") + "]"
-//
-//	reqSpan, spanErr := StartSpantoSkyWalkingForDb(queryStr+"\r\n Parameters: "+result, os.Getenv("DB_URL"))
-//	if spanErr != nil {
-//		log.Printf("StartSpantoSkyWalkingForDb error: %v \n", spanErr)
-//	}
-//
-//	rows, err := query.RunWith(f.getDb()).Query()
-//
-//	if err != nil {
-//		EndSpantoSkywalkingForDb(reqSpan, queryStr, false, err)
-//	}
-//
-//	EndSpantoSkywalkingForDb(reqSpan, queryStr, true, err)
-//
-//	return rows, err
-//}
-//
-//func StartSpantoSkyWalkingForDb(queryStr string, db string) (go2sky.Span, error) {
-//	originCtx := GetContext()
-//	if originCtx == nil {
-//		return nil, errors.New("can not get context")
-//	}
-//	ctx := originCtx.(echo.Context)
-//	// op_name 是每一个操作的名称
-//	tracerFromCtx := ctx.Get("tracer")
-//	if tracerFromCtx == nil {
-//		return nil, errors.New("can not get tracer")
-//	}
-//	tracer := tracerFromCtx.(*go2sky.Tracer)
-//	reqSpan, err := tracer.CreateExitSpan(ctx.Request().Context(), queryStr, db, func(header string) error {
-//		ctx.Get("header").(*SafeHeader).Set(propagation.Header, header)
-//		return nil
-//	})
-//	reqSpan.SetComponent(5)
-//	reqSpan.SetSpanLayer(v3.SpanLayer_Database) // rpc 调用
-//	reqSpan.Log(time.Now(), "[DBRecord Request]", fmt.Sprintf("开始请求,请求服务:%s,请求地址:%s", db, queryStr))
-//
-//	return reqSpan, err
-//}
-//
-//func EndSpantoSkywalkingForDb(reqSpan go2sky.Span, queryStr string, isNormal bool, err error) {
-//	if reqSpan == nil {
-//		return
-//	}
-//	reqSpan.Tag(go2sky.TagDBType, "MySql")
-//	reqSpan.Tag(go2sky.TagURL, queryStr)
-//	if !isNormal {
-//		reqSpan.Error(time.Now(), "[DBRecord Response]", fmt.Sprintf("结束请求,响应结果: %s", err))
-//	} else {
-//		reqSpan.Log(time.Now(), "[DBRecord Response]", "结束请求")
-//	}
-//	reqSpan.End()
-//}

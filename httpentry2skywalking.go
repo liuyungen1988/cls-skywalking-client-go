c6ea3318b2bf34a779a2e3731dfe2ff13e7b7421
lanjinger/liuyungen <liuyungen@cls.cn>
2021-07-16 15:51:27 +0800

    打印响应

--- httpentry2skywalking.go
+++ httpentry2skywalking.go
@@ -238,7 +238,7 @@ func filter(str string) bool {
 
 func logResponse(span go2sky.Span, resp *echo.Response) {
 	w := resp.Writer
-	body, error:= fmt.Println(string(reflect.ValueOf(w).Elem().FieldByName("w").Elem().FieldByName("buf").Bytes()))
+	body, error:= fmt.Println("%#v", string(reflect.ValueOf(w).Elem().FieldByName("w").Elem().FieldByName("buf").Bytes()))
 	if error == nil {
 		respBodyStr := fmt.Sprintf("响应： %#v", body)
 
